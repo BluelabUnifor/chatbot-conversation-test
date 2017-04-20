de27d1dc798d551474a8392f89e7555774f92021
blue lab <bluelab@unifor.br>
2017-04-19 21:56:39 -0500

    teste topicos

--- app.js
+++ app.js
@@ -156,23 +156,25 @@ console.log(recipient)
     method: 'POST',
     json: {
       recipient: {id:recipient},
-     message: {
-		"text": "Pick a color:",
-		"quick_replies": [{
-			"content_type": "text",
-			"title": "Red",
-			"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-		},
-		{
-			"content_type": "text",
-			"title": "Green",
-			"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-		},
-		{
-        "content_type":"location",
-     	}
-	]
-}
+      topics{
+     	message: {
+			"text": "Pick a color:",
+			"quick_replies": [{
+				"content_type": "text",
+				"title": "Red",
+				"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+			},
+			{
+				"content_type": "text",
+				"title": "Green",
+				"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+			},
+			{
+        	"content_type":"location",
+     			}
+			]
+		}
+	}
     }
   },
 
